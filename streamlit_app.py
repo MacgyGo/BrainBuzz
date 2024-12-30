04965e208f0f01c54ed4bab5547f4a88683af300
MacgyGo <ebatesshopca01@gmail.com>
2024-12-30 14:21:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,18 +9,32 @@ def main():
     st.title("Brain Buzz")
 
     if 'quiz' not in st.session_state:
+        choose_question_count()
+    elif not st.session_state.quiz_started:
         initialize_quiz()
-
-    if st.session_state.quiz.has_questions():
+    elif st.session_state.quiz.has_questions():
         display_question()
     else:
         display_results()
+
+def choose_question_count():
+    st.session_state.quiz_started = False
+    question_data = get_questions()
+    max_questions = len(question_data)
+    st.write(f"Total available questions: {max_questions}")
+    question_count = st.selectbox("Choose the number of questions:", 
+                                  options=[10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+                                  index=0)
+    if st.button("Start Quiz"):
+        st.session_state.question_count = min(question_count, max_questions)
+        st.session_state.quiz_started = True
+        st.experimental_rerun()
 
 def initialize_quiz():
     question_data = get_questions()
     question_bank = [Question(q['question'], q['incorrect_answers'] + [q['correct_answer']], q['correct_answer']) for q in question_data]
     st.session_state.quiz = QuizBrain(question_bank)
-    st.session_state.quiz.set_question_number(10)  # Default to 10 questions, you can make this configurable
+    st.session_state.quiz.set_question_number(st.session_state.question_count)
     st.session_state.current_question = st.session_state.quiz.next_question()
     st.session_state.time_left = 30
     st.session_state.answered = False
@@ -78,7 +92,6 @@ def display_results():
         st.experimental_rerun()
 
 def get_random_light_color():
-    # Generate a random light color
     r = random.randint(200, 255)
     g = random.randint(200, 255)
     b = random.randint(200, 255)
