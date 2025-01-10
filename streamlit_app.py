b55f8972dd4c6b96dd41eed995ed90529b0d201c
MacgyGo <ebatesshopca01@gmail.com>
2025-01-09 20:59:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,16 +4,6 @@ from quiz_data import get_questions
 from question_model import Question
 from quiz_brain import QuizBrain
 
-import time
-
-st.set_page_config()
-
-ph = st.empty()
-N = 5*60
-for secs in range(N,0,-1):
-    mm, ss = secs//60, secs%60
-    ph.metric("Countdown", f"{mm:02d}:{ss:02d}")
-    time.sleep(1)
 def get_random_light_color():
     """Generates a random light color in RGB format"""
     r = random.randint(200, 255)
